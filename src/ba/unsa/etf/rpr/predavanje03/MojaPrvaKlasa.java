1d7643129d53c871cdf755b606a046fb979408b7
GreenChicken21 <dcukurija1@etf.unsa.ba>
2018-10-14 00:04:12 +0200

    Ispisicanje n brojeva gdje n unosi korisnik

--- src/ba/unsa/etf/rpr/predavanje03/MojaPrvaKlasa.java
+++ src/ba/unsa/etf/rpr/predavanje03/MojaPrvaKlasa.java
@@ -1,9 +1,16 @@
 package ba.unsa.etf.rpr.predavanje03;
+import java.util.Scanner;
 
 public class MojaPrvaKlasa {
 
     public static void main(String[] args) {
 	// write your code here
-
+        System.out.print("Unesite broj n: ");
+        Scanner reader = new Scanner( System.in );
+        int n = reader.nextInt();
+        for(int i = 0; i < n; i++){
+            System.out.print(" " + (i+1));
+        }
+        reader.close();
     }
 }
